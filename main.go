afa066f218228f064132b69608938e6416e8c9fe
Rimgaudas Jurgaitis <rimgaudas.jurgaitis@gmail.com>
2025-05-12 21:07:40 +0300

    progress through lesson

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 	"sync/atomic"
@@ -11,27 +13,20 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	cfg := &apiConfig{
+	cfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", handleHealtz)
-	mux.HandleFunc("/metrics", cfg.handlerMetrics)
-	mux.HandleFunc("/reset", cfg.handlerReset)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", handleHealtz)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
 
 	server := &http.Server{
 		Handler: mux,
@@ -40,22 +35,37 @@ func main() {
 	server.ListenAndServe()
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func handleHealtz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	resp := fmt.Sprintf("Hits: %v", strconv.Itoa(int(cfg.fileserverHits.Load())))
-	w.Write([]byte(resp))
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	count := strconv.Itoa(int(cfg.fileserverHits.Load()))
+	tmpl, err := template.ParseFiles("./admin-metrics.html")
+	if err != nil {
+		log.Fatalf("error parsing template: %v", err)
+	}
+
+	err = tmpl.Execute(w, count)
+	if err != nil {
+		log.Fatalf("error executing template: %v", err)
+	}
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	resp := fmt.Sprintf("Hits: %v", strconv.Itoa(int(cfg.fileserverHits.Swap(0))))
 	w.Write([]byte(resp))
 }
